fc15cc56e6277cfb641b53629401688c008d9c93
Chekwube <chekwube@rubikpay.net>
2021-01-30 15:32:22 +0100

    Added tests

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ app.use(errorHandler());
 /**
  * Start Express server.
  */
-const server = app
+export const server = app
   .listen(app.get('port'), () => {
     console.log('  App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'));
     console.log('  Press CTRL-C to stop\n');
@@ -19,4 +19,4 @@ const server = app
     console.log(err);
   });
 
-export default server;
+// export default server;
